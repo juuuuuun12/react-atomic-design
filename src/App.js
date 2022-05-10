e7adc362f7a74a75691f932cd7e4715ae8304f79
juuuuuun12 <104136084+juuuuuun12@users.noreply.github.com>
2022-05-10 23:29:52 +0900

    create template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 
 import "./styles.css";
 
@@ -17,12 +20,14 @@ const user = {
 };
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>test</PrimaryButton>
-      <SecondaryButton>push</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>test</PrimaryButton>
+        <SecondaryButton>push</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
