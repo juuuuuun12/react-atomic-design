14f4bf31e95008864a6f65ee082c5cb1745cf774
juuuuuun12 <104136084+juuuuuun12@users.noreply.github.com>
2022-05-10 23:14:28 +0900

    create organisms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 
 import "./styles.css";
 
+const user = {
+  name: "じゅん",
+  image: "https://source.unsplash.com/g5ZM33jizAw",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト会社"
+  },
+  website: "https://google.com"
+};
 export default function App() {
   return (
     <div className="App">
@@ -11,6 +22,7 @@ export default function App() {
       <SecondaryButton>push</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
